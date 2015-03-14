55d977bc0aa7b12246cd7562fa6edf77780866b1
Matthew Franglen <matthew@franglen.org>
2015-03-14 22:42:32 +0000

    Complete transition of tests to new structure

--- test/beans/models.dart
+++ test/beans/models.dart
@@ -41,4 +41,83 @@ class _CreationExceptionThrowingConfiguration extends AbstractInjectConfiguratio
   @bean String createBean() => throw new Exception();
 }
 
+abstract class _AutowiredBean {
+  bool get beanHasBeenAutowired;
+}
+
+class _AutowiredFieldBean implements _AutowiredBean {
+  @autowired String bean;
+
+  _AutowiredFieldBean() : bean = null;
+
+  bool get beanHasBeenAutowired => bean != null;
+}
+
+class _AutowiredSetterBean implements _AutowiredBean {
+
+  bool beanHasBeenAutowired;
+
+  _AutowiredSetterBean() : beanHasBeenAutowired = false;
+
+  @autowired set mockBean(String bean) {
+    beanHasBeenAutowired = true;
+  }
+}
+
+class _AutowiredMethodBean implements _AutowiredBean {
+
+  bool beanHasBeenAutowired;
+
+  _AutowiredMethodBean() : beanHasBeenAutowired = false;
+
+  @autowired void setBean(String bean) {
+    beanHasBeenAutowired = true;
+  }
+}
+
+abstract class _AutowiredBeanConfiguration extends AbstractInjectConfiguration {
+  _AutowiredBean get autowiredBean;
+
+  bool get beanHasBeenCreated => autowiredBean != null;
+}
+
+class _AutowiredBeanFieldConfiguration extends _AutowiredBeanConfiguration {
+
+  @autowired _AutowiredBean autowiredBean;
+
+  @bean _AutowiredBean createBean() {
+    return new _AutowiredFieldBean();
+  }
+
+  @bean String createMockBean() {
+    return 'bean';
+  }
+}
+
+class _AutowiredBeanSetterConfiguration extends _AutowiredBeanConfiguration {
+
+  @autowired _AutowiredBean autowiredBean;
+
+  @bean _AutowiredBean createBean() {
+    return new _AutowiredSetterBean();
+  }
+
+  @bean String createMockBean() {
+    return 'bean';
+  }
+}
+
+class _AutowiredBeanMethodConfiguration extends _AutowiredBeanConfiguration {
+
+  @autowired _AutowiredBean autowiredBean;
+
+  @bean _AutowiredBean createBean() {
+    return new _AutowiredMethodBean();
+  }
+
+  @bean String createMockBean() {
+    return 'bean';
+  }
+}
+
 // vim: set ai et sw=2 syntax=dart :
--- test/beans/test.dart
+++ test/beans/test.dart
@@ -5,10 +5,12 @@ import 'package:dependency_injection/dependency_injection.dart';
 import '../behave/behave.dart';
 
 part 'models.dart';
+part 'test_autowiring.dart';
 part 'test_creation.dart';
 
 void test() {
   testCreation();
+  testAutowiring();
 }
 
 // vim: set ai et sw=2 syntax=dart :
