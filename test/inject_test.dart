ad2bf094ea64ee175dd9b61675fb380dd2a663cc
Matthew Franglen <matthew@franglen.org>
2015-03-14 13:09:41 +0000

    Add tests for autowiring on beans

--- test/inject_test.dart
+++ test/inject_test.dart
@@ -314,6 +314,38 @@ void main() {
     );
   });
 
+  group('Given a @Configuration class instance with a @Bean that has an @Autowired field${nl}', () {
+    BeanAutowiringConfiguration configuration;
+
+    setUp(() {
+      configuration = new BeanFieldAutowiringConfiguration();
+    });
+    test('When I configure the instance${nl} Then the bean field is autowired',
+      () => when(triggerConfiguration(configuration)).then(beansHaveBeenInternallyAutowired)
+    );
+  });
+
+  group('Given a @Configuration class instance with a @Bean that has an @Autowired setter${nl}', () {
+    BeanAutowiringConfiguration configuration;
+
+    setUp(() {
+      configuration = new BeanSetterAutowiringConfiguration();
+    });
+    test('When I configure the instance${nl} Then the bean setter is autowired',
+      () => when(triggerConfiguration(configuration)).then(beansHaveBeenInternallyAutowired)
+    );
+  });
+
+  group('Given a @Configuration class instance with a @Bean that has an @Autowired method${nl}', () {
+    BeanAutowiringConfiguration configuration;
+
+    setUp(() {
+      configuration = new BeanMethodAutowiringConfiguration();
+    });
+    test('When I configure the instance${nl} Then the bean method is autowired',
+      () => when(triggerConfiguration(configuration)).then(beansHaveBeenInternallyAutowired)
+    );
+  });
 }
 
 typedef dynamic Clause();
@@ -339,6 +371,10 @@ void beansHaveBeenCreated(MockConfiguration configuration) {
 
 void beansHaveBeenAutowired(MockConfiguration configuration) {
   expect(configuration.beanHasBeenAutowired, isTrue);
+}
+
+void beansHaveBeenInternallyAutowired(BeanAutowiringConfiguration configuration) {
+  expect(configuration.autowiredBean.beanHasBeenAutowired, isTrue);
 }
 
 // vim: set ai et sw=2 syntax=dart :
--- test/test_data.dart
+++ test/test_data.dart
@@ -318,4 +318,87 @@ class MultiplePrimaryQualifierAutowiredBeanConfiguration extends AbstractInjectC
 
 class MockBean {}
 
+abstract class AutowiredBean {
+  bool get beanHasBeenAutowired;
+}
+
+class AutowiredFieldBean implements AutowiredBean {
+  @autowired MockBean bean;
+
+  AutowiredFieldBean() : bean = null;
+
+  bool get beanHasBeenAutowired => bean != null;
+}
+
+class AutowiredSetterBean implements AutowiredBean {
+
+  bool beanHasBeenAutowired;
+
+  AutowiredSetterBean() : beanHasBeenAutowired = false;
+
+  @autowired set mockBean(MockBean bean) {
+    beanHasBeenAutowired = true;
+  }
+}
+
+class AutowiredMethodBean implements AutowiredBean {
+
+  bool beanHasBeenAutowired;
+
+  AutowiredMethodBean() : beanHasBeenAutowired = false;
+
+  @autowired void setBean(MockBean bean) {
+    beanHasBeenAutowired = true;
+  }
+}
+
+abstract class BeanAutowiringConfiguration extends AbstractInjectConfiguration {
+  AutowiredBean get autowiredBean;
+}
+
+class BeanFieldAutowiringConfiguration extends BeanAutowiringConfiguration {
+
+  @autowired AutowiredBean autowiredBean;
+
+  BeanFieldAutowiringConfiguration();
+
+  @bean AutowiredBean createBean() {
+    return new AutowiredFieldBean();
+  }
+
+  @bean MockBean createMockBean() {
+    return new MockBean();
+  }
+}
+
+class BeanSetterAutowiringConfiguration extends BeanAutowiringConfiguration {
+
+  @autowired AutowiredBean autowiredBean;
+
+  BeanSetterAutowiringConfiguration();
+
+  @bean AutowiredBean createBean() {
+    return new AutowiredSetterBean();
+  }
+
+  @bean MockBean createMockBean() {
+    return new MockBean();
+  }
+}
+
+class BeanMethodAutowiringConfiguration extends BeanAutowiringConfiguration {
+
+  @autowired AutowiredBean autowiredBean;
+
+  BeanMethodAutowiringConfiguration();
+
+  @bean AutowiredBean createBean() {
+    return new AutowiredMethodBean();
+  }
+
+  @bean MockBean createMockBean() {
+    return new MockBean();
+  }
+}
+
 // vim: set ai et sw=2 syntax=dart :
