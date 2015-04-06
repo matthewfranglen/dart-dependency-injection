94b089ee0d5a488657ffd204c831e5236c1da0a8
Matthew Franglen <matthew@franglen.org>
2015-04-06 14:31:09 +0100

    Use logging instead of print

--- lib/dependency_injection.dart
+++ lib/dependency_injection.dart
@@ -1,11 +1,14 @@
 library dependency_injection;
 
 import 'dart:mirrors';
+import 'package:logging/logging.dart';
 import 'package:annotate/annotate.dart';
 import 'package:patch_mirrors/patch_mirrors.dart' as patch;
 
 part 'src/annotations.dart';
 part 'src/dependency_injection.dart';
 part 'src/models.dart';
+
+final Logger _log = new Logger('dependency_injection');
 
 // vim: set ai et sw=2 syntax=dart :
